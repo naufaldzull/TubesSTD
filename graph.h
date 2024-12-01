7d223945498b2a167188e6d2dec45da653dfef77
dinasalsabilla <dinasalsabilla05@gmail.com>
2024-12-01 21:54:29 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,44 +1,36 @@
 #ifndef GRAPH_H_INCLUDED
 #define GRAPH_H_INCLUDED
 
-#define idVertex(V) V->idVertex
-#define nextVertex(V) V->nextVertex
-#define firstVertex(G) G.firstVertex
-#define firstEdge(E)  E->firstEdge
-#define nextEdge(E) E->nextEdge
-
 #include <iostream>
-
+#include <string>
+#define firstVertex(G) G.firstVertex
+#define firstEdge(V) V->firstEdge
+#define nextVertex(V) V->nextVertex
+#define nextEdge(E) E->nextEdge
+#define idVertex(V) V->idVertex
+#define destVertexID(E) E->destVertexID
+#define weight(E) E->weight
 using namespace std;
-
-struct Edge;
-struct Vertex;
-typedef Vertex *adrVertex;
-typedef edge *adrEdge;
-
-struct Vertex {
+typedef struct vertex *adrVertex;
+typedef struct edge *adrEdge;
+struct vertex {
     char idVertex;
     adrVertex nextVertex;
     adrEdge firstEdge;
 };
-
-struct Edge {
+struct edge {
     char destVertexID;
     int weight;
     adrEdge nextEdge;
 };
-
-struct Graph {
+struct graph {
     adrVertex firstVertex;
 };
 
-
-void createVertex(char newVertexID, adrVertex &V);
-void initGraph(Graph &G);
-void addVertex(Graph &G, char newVertexID);
-void buildGraph(Graph &G);
-void addEdge(Graph &G, char fromVertexID, char toVertexID, int weight);
-
-
+void createVertex(char newVertexID, adrVertex &v);
+void initGraph(graph &G);
+void addVertex(graph &G, char newVertexID);
+// void addEdge(Graph &G, char fromVertexID, char toVertexID, int weight);
+void buildGraph(graph &G);
 
 #endif // GRAPH_H_INCLUDED
