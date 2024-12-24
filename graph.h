ee5343c1561ce0b39fa1b2b2ecb977f474e91552
dinasalsabilla <dinasalsabilla05@gmail.com>
2024-12-24 18:36:30 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -3,55 +3,40 @@
 
 #include <iostream>
 #include <string>
-#define info(V) V->info
 #define firstVertex(G) G.firstVertex
 #define firstEdge(V) V->firstEdge
 #define nextVertex(V) V->nextVertex
 #define nextEdge(E) E->nextEdge
-#define idVertex(V) V->idVertex
-#define destVertexID(E) E->destVertexID
-#define weight(E) E->weight
+#define gedung(V) V->gedung
+#define destGedung(E) E->destGedung
+#define jarak(E) E->jarak
 using namespace std;
-
-struct infotypeGedung{
-    string namaGedung;
-};
-
-struct infoTypeJalan{
-    int weight; //jaraknya
-};
-
-typedef struct elmntGedung *adrGedung;
-typedef struct edge *adrJalan;
-
-struct elmntGedung {
-    infotypeGedung info; // Informasi gedung
+typedef struct elmGedung *adrGedung;
+typedef struct elmJalan *adrJalan;
+struct elmGedung {
+    string gedung;
     adrGedung nextVertex;
     adrJalan firstEdge;
 };
-
-struct elmntJalan {
-    infoTypeJalan info;
-    string destVertexID;
+struct elmJalan {
+    int jarak;
+    string destGedung;
     adrJalan nextEdge;
 };
-
 struct graph {
     adrGedung firstVertex;
 };
 
-// Fungsi dan Prosedur
-void initGraph(graph &G); // Inisialisasi graph kosong
-adrGedung allocateVertex(infotypeGedung info); // Alokasi memori untuk vertex
-adrJalan allocateEdge(string destVertexID, int weight); // Alokasi memori untuk jalan
-void addVertex(graph &G, string newVertexID); // Menambahkan vertex baru ke graph
-void addEdge(graph &G, string fromVertexID, string toVertexID, int weight); // Menambahkan edge (jalan) ke graph
-void buildGraph(graph &G); // Bangun graph dari input pengguna
-void cetakNavigasi(graph G); // Cetak seluruh graph
-adrGedung findVertex(graph G, string vertexID);
-void cariTetangga(graph G, string vertexID); // Menampilkan tetangga vertex tertentu
-void hapus(graph &G); // Menghapus semua vertex dan edge dalam graph
-void ruteTerdekat(graph G, string startVertexID, string endVertexID);
-
+void initGraph(graph &G);
+adrGedung alokasiGedung(string newGedung);
+void addGedung(graph &G, string newGedung);
+adrJalan alokasiJalan(string destGedung, int jarak);
+void addJalan(graph &G, string fromGedung, string toGedung, int jarak);
+void printNavigasi(graph G)
+adrGedung findGedung(graph G, string gedung);
+void findTetangga(graph G, string gedung);
+... ruteTerdekat(...);
+void delGedung(graph &G, string gedung);
+void delJalan(graph &G, string fromGedung, string toGedung);
 
 #endif // GRAPH_H_INCLUDED
