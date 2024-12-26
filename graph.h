ba152adc06aefe8e93960e6e664e4425746cd4ef
dinasalsabilla <dinasalsabilla05@gmail.com>
2024-12-26 18:41:14 +0700

    Update graph.h

--- graph.h
+++ graph.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <map>
+#include <climits>
 #define firstVertex(G) G.firstVertex
 #define firstEdge(V) V->firstEdge
 #define nextVertex(V) V->nextVertex
@@ -10,6 +12,8 @@
 #define gedung(V) V->gedung
 #define destGedung(E) E->destGedung
 #define jarak(E) E->jarak
+#define priority(N) N->priority
+#define next(N) N->next
 using namespace std;
 
 typedef struct elmGedung *adrGedung;
@@ -28,6 +32,16 @@ struct graph {
     adrGedung firstVertex;
 };
 
+typedef struct node *adrNode;
+struct node {
+    adrGedung gedung;
+    int priority;
+    adrNode next;
+};
+struct PQ {
+    adrNode head;
+};
+
 void initGraph(graph &G);
 adrGedung alokasiGedung(string gedung);
 void addGedung(graph &G, string gedung);
@@ -38,8 +52,15 @@ void addJalan(graph &G, string fromGedung, string toGedung, int jarak);
 adrJalan findJalan(adrGedung V, string toGedung);
 void showJalan(graph G);
 void findTetangga(graph G, string gedung);
-// ... ruteTerpendek(...);
 void delGedung(graph &G, string gedung);
 void delJalan(graph &G, string fromGedung, string toGedung);
+
+void initPQ(PQ &pq);
+void pushPQ(PQ &pq, adrGedung gedung, int priority);
+adrGedung popPQ(PQ &pq);
+bool isEmptyPQ(PQ pq);
+void cetakRute(adrGedung gedung, map<adrGedung, adrGedung> &prev);
+void ruteTerpendek(graph G, string fromGedung, string toGedung);
+
 
 #endif // GRAPH_H_INCLUDED
